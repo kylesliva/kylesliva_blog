e7a7c25d06b1f13cbd71933091db012e68957b67
Kyle Sliva <kyle@kylesliva.me>
2022-01-01 18:57:27 -0500

    remove .terraform

--- main.tf
+++ main.tf
@@ -16,4 +16,8 @@ provider "aws" {
 resource "aws_instance" "test_server" {
     ami = "ami-002068ed284fb165b"
     instance_type = "t3a.nano"
+    tags = {
+        Name = var.server_name
+    }
+    subnet_id = var.subnet
 }
