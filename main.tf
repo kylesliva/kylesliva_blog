9b226a1dc6efee500a62a21dd2dcfc363abff9e5
Kyle Sliva <kyle@kylesliva.me>
2022-01-01 21:56:36 -0500

    add S3 bucket

--- main.tf
+++ main.tf
@@ -13,11 +13,49 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "test_server" {
+resource "aws_instance" "blog_server" {
     ami = "ami-002068ed284fb165b"
-    instance_type = "t3a.nano"
+    instance_type = "t3a.micro"
     tags = {
         Name = var.server_name
+        blog = "true"
     }
     subnet_id = var.subnet
+    key_name = var.keypair
+    associate_public_ip_address = "true"
+    vpc_security_group_ids = [
+        aws_security_group.webports.id,
+        var.mgmtsg
+    ]
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "kylesliva-blog-resources"
+    acl = "public-read"
+     server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "aws:kms"
+                kms_master_key_id = aws_kms_key.s3_kms.arn
+            }
+        }
+    }
+}
+
+resource aws_kms_key "s3_kms" {
+    description = "KMS key for personal blog site's S3 bucket"
+    enable_key_rotation = "true"
+}
+
+resource "aws_security_group" "webports" {
+    name = "webaccess"
+    description = "Allow traffic on port 4000/tcp"
+    vpc_id = var.webvpc
+    ingress {
+        description = "port 4000 ingress"
+        from_port = 4000
+        to_port = 4000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
--- variables.tf
+++ variables.tf
@@ -4,8 +4,26 @@ variable "server_name" {
     default = "blog_server"
 }
 
+variable "keypair" {
+    description = "keypair for blog EC2"
+    type = string
+    default = "blog_keypair"
+}
+
 variable "subnet" {
     description = "Subnet to use for blog"
     type = string
     default = "subnet-06cedc805b1fba76b"
+}
+
+variable "webvpc" {
+    description = "target VPC"
+    type = string
+    default = "vpc-040a352b8c288be4f"
+}
+
+variable "mgmtsg" {
+    description = "SSH SG"
+    type = string
+    default = "sg-0eceea5b6a0370ad9"
 }
